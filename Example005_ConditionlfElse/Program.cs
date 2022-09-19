175fe23b6cacef35974009e5c408ac7d23c7d3dc
Sergey <serqey2020@outlook.com>
2022-09-19 19:15:23 +0300

    ConditionElse

---
+++ Example005_ConditionlfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Сергей")
+{
+    Console.WriteLine("Ура, это же Сергей!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
